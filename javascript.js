62677e6f5b7c2b2ab4297f93a5fc35b619a53c69
Saksham Pandey <pandeysaksham25@gmail.com>
2020-09-19 11:34:20 +0530

    added js

---
+++ javascript.js
@@ -0,0 +1,30 @@
+var objPeople = [
+    {
+        username:"pandey.saksham0",
+        password:"123456"
+    },
+
+    {
+        username:"saksham.pandey0",
+        password:"012345"
+    },
+
+
+    {
+        username:"deepak",
+        password:"12345"
+    }
+]
+
+function getinfo(){
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+    for(let i=0;i<objPeople.length;i++)
+    {
+        if(username===objPeople[i].username&&password===objPeople[i].password)
+        {
+            location="login.html"
+        }
+    }
+    
+}
